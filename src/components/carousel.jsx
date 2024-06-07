f5ba36333afea4cd490870e8db40ee7d01c9f910
SopanhaSem <semsopanha072@gmail.com>
2024-06-07 23:52:50 +0700

    final

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scarousel = () => {
   return (
-    <div class="grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2 bg-white dark:bg-gray-800 mt-5">
+    <div class=" grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2 bg-white dark:bg-gray-800 mt-5">
       <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
         <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
           <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -51,7 +51,7 @@ const NavBar = () => {
   }, []);
 
   return (
-    <header className="">
+    <header>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
